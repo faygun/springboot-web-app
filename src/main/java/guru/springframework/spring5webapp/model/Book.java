46079c8cb9bc5431cbf1bc8c5d372d67ab0f53ee
Ahmet Faruk Aygün <faygun89@gmail.com>
2018-12-17 19:15:10 +0300

    Added respository and bootstrap

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -12,7 +12,9 @@ public class Book {
     private  Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -23,13 +25,13 @@ public class Book {
 
     }
 
-    public Book(String title, String isbn, String publisher){
+    public Book(String title, String isbn, Publisher publisher){
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors){
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors){
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -52,7 +54,7 @@ public class Book {
         return isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
@@ -68,7 +70,7 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
